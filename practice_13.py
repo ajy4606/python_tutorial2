c915acd4e81e6fde8efea310e0c00747d39b7760
Ahnjy <ajy4606@naver.com>
2023-11-17 14:45:46 +0900

    while

--- practice_13.py
+++ practice_13.py
@@ -1,11 +1,24 @@
 # for 반복문
 # randrange()
-from random import *
-print(randrange(1, 5))
+# from random import *
+# print(randrange(1, 5))
 
-for waiting_num in range(1,6):
-    print("대기번호 : {0}".format(waiting_num))
+# for waiting_num in range(1,6):
+#     print("대기번호 : {0}".format(waiting_num))
 
-starbucks = ["아이언맨", "토르", "아이엠그루트"]
-for customer in starbucks:
-    print("{0}, 커피가 준비되었습니다.".format(customer))
+# starbucks = ["아이언맨", "토르", "아이엠그루트"]
+# for customer in starbucks:
+#     print("{0}, 커피가 준비되었습니다.".format(customer))
+
+# customer = "아이언맨"
+# index = 1
+# while True:
+#     print("{0}, 커피가 준비되었습니다. 호출 {1} 회".format(customer, index))
+#     index += 1
+
+customer = "토르"
+person = "Unknown"
+
+while person != customer :
+    print("{0}, 커피가 준비 되었습니다.".format(customer))
+    person = input("이름이 어떻게 되세요? ")
