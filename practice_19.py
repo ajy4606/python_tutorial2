efce62f55169000e32cc83028988fc1c04f39028
Ahnjy <ajy4606@naver.com>
2023-11-18 13:28:46 +0900

    입출력

---
+++ practice_19.py
@@ -0,0 +1,23 @@
+#키워드 값
+
+def profile(name, age, main_lang):
+    print(name, age, main_lang)
+
+profile(name="유재석", main_lang="파이썬", age=20)
+profile(main_lang="자바", age=25, name="김태호")
+profile("정준하", 10, "자바")
+
+# 가변인자
+
+# def profile(name, age, lang1, lang2, lang3, lang4, lang5):
+#     print("이름 : {0}\t나이: {1}\t".format(name, age), end=" ")
+#     print(lang1, lang2, lang3, lang4, lang5)
+
+def profile(name, age, *language):
+    print("이름 : {0}\t나이: {1}\t".format(name, age), end=" ")
+    for lang in language:
+        print(lang, end=" ")
+    print()
+
+profile("유재석", 20, "Python", "Java", "C++", "C#", "C", "JavaScript")
+profile("김태호", 25, "Kotlin", "Swift")
